17d44ffc5a0463bc3b4a1ab08132f66850cdc80b
nadiaqueen517 <nadiaqueen517@gmail.com>
2025-02-01 20:06:55 +0500

    execute.py

--- execute.py
+++ execute.py
@@ -1,1 +1,8 @@
-import os, platform
+import os, platform, time
+aarch = platform.architecture()[0]
+if "64bit" in aarch:
+  os.system("clear")
+  print(" \x1b[1;92mtool loading... ")
+  import itssecret
+else:
+  print(" \x1b[1;91merror ")
